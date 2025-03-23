d0446598fe59c23f6805986f4bcd9212cc7ee53d
KANAKA <kanakadh2004@gmail.com>
2025-03-23 10:26:14 +0530

    commit "first commit"

---
+++ function.js
@@ -0,0 +1,70 @@
+
+// normal function//
+// function sum(a,b){
+//     let c=a*b
+//     console.log(c)
+// }
+// sum(5,2)
+
+
+// sum(5,2)
+// function sum(a,b){
+//     let c=a*b
+//     console.log(c)
+// }
+
+
+
+// arrow function
+// const sum=(a,b)=>{
+//     var c=a*b
+//     console.log(c)
+// }
+// sum(5,6)
+
+
+// sum(5,6)
+
+// const sum=(a,b)=>{
+//     var c=a*b
+//     console.log(c)
+// }
+
+
+// callback
+// const sum=(a,b)=>{
+//     var c=a*b
+//     setTimeout(( )=>{console.log(c)},1000)
+// }
+// sum(5,6);
+
+
+const display=()=>{
+    var a=10
+    var b=20
+    var c=30
+    console.log(a);
+    setTimeout(( )=>{console.log(b)},2000);
+    console.log(c);
+}
+display();
+
+// IIEF
+//  (function sum(a,b){
+//     var c=a+b
+//     console.log(c);
+// }
+// )
+// (5,2)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ hello.js
@@ -0,0 +1,15 @@
+// console.log("heelo js from external file..");
+
+
+// document.write("helllo java script..");
+  
+//document.write("hii....");
+
+//window.alert("hello");
+
+//console.log("message");
+
+//alert("");
+
+alert("this is first line.\n this is second line");
+
