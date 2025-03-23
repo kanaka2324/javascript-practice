2825370ebb38daf0c06aec860ca06278c972d5a3
KANAKA <kanakadh2004@gmail.com>
2025-03-23 10:25:03 +0530

    first commit

---
+++ variables.js
@@ -0,0 +1,72 @@
+// let x=10
+// var y=20
+// const z=30
+
+// console.log(x,y,z)
+
+
+
+// var x=10
+// {
+//     let y=20
+//     const z=30
+//     console.log(x,y,z)
+// }
+// console.log(x,y,z)
+
+
+
+// var b=10
+// var b=20
+// console.log(b)
+
+
+
+// let a=10
+// let a=20
+// console.log(a)
+
+
+
+// const c=10
+// const c=20
+// console.log(c)
+
+
+
+// var b=10
+// b=20
+// console.log(b)
+
+
+
+// let a=10
+// a=20
+// console.log(a)
+
+
+
+// const c=10
+// c=20
+// console.log(c)
+
+
+
+// console.log(a);
+// var a=10
+
+
+
+// console.log(b)
+// let b=10
+
+
+
+// console.log(c)
+// const c=10
+
+
+
+
+
+
