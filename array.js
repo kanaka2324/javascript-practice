37ff6624367b0c5edfa5efc09a87251bd1aa53ba
KANAKA <kanakadh2004@gmail.com>
2025-04-01 19:16:00 +0530

    this is code

---
+++ array.js
@@ -0,0 +1,126 @@
+// let arr=[1,2,3,4,5];
+// for(let i=0;i<arr.length;i++)
+// {
+// console.log(arr[i]+"");
+// }
+
+
+
+// dry run
+// let arr=[1,2,3,4,5];
+// for(let i=0;i<5;i++)
+// {
+// console.log(arr[i]+"");
+// }
+
+
+// let arr=[1,2,3,4,5];
+// for(let i=1;i<5;i++)
+// {
+// console.log(arr[i]+"");
+// }
+
+// let arr=[1,2,3,4,5];
+// for(let i=2;i<5;i++)
+// {
+// console.log(arr[i]+"");
+// }
+
+
+// let arr=[1,2,3,4,5];
+// for(let i=3;i<5;i++)
+// {
+// console.log(arr[i]+"");
+// }
+
+// let arr=[1,2,3,4,5];
+// for(let i=4;i<5;i++)
+// {
+// console.log(arr[i]+"");
+// }
+
+// condtion false
+// let arr=[1,2,3,4,5];
+// for(let i=5;i<5;i++)
+// {
+// console.log(arr[i]+"");
+// }
+
+
+//REVERSE AN ARRAY
+// let array=[1,2,3,4,5];
+// for(let i=5;i>=0;i--)
+// {
+// console.log(array[i]+"");
+// }
+
+// let array=[1,2,3,4,5];
+// for(let i=5;i>=1;i--)
+// {
+// console.log(array[i]+"");
+// }
+
+// let array=[1,2,3,4,5];
+// for(let i=5;i>=2;i--)
+// {
+// console.log(array[i]+"");
+// }
+
+// let array=[1,2,3,4,5];
+// for(let i=5;i>=3;i--)
+// {
+// console.log(array[i]+"");
+// }
+
+
+// let array=[1,2,3,4,5];
+// for(let i=5;i>=4;i--)
+// {
+// console.log(array[i]+"");
+// }
+
+
+// let array=[1,2,3,4,5];
+// for(let i=5;i>=5;i--)
+// {
+// console.log(array[i]+"");
+// }
+
+// let arr=[1,2,3,4,5];
+// arr.push(6);  //push the element last index
+// console.log(arr);
+
+// let arr=[1,2,3,4,5];
+// arr.shift(1); //delete first element
+// console.log(arr);
+
+// let arr=[1,2,3,4,5];
+// arr.unshift(0);//add element in first
+// console.log(arr);
+
+// let arr=[1,2,3,4,5];
+// let slicearr=arr.slice(1,5);//.slice(first index,last index)
+// console.log(slicearr);
+
+// let colors=["red","green","blue","yellow"];
+// let removed=colors.splice(1,2);//remove the element
+// console.log(colors);
+// console.log(removed);
+
+
+
+// for each
+//array--> we use of
+// let arr=[1,2,3,4,5];
+
+// for(let num of arr){
+//     console.log(num);
+// }
+
+// OR
+
+// let numbers=[1,2,3,4,5];
+// numbers.forEach(function(num){
+//     console.log(num);
+// });
+
