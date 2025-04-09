64189547ba3506253bd0be8b9dfbc3f2edbaa43b
KANAKA <kanakadh2004@gmail.com>
2025-04-09 09:47:36 +0530

    commit the code

---
+++ loops.js
@@ -0,0 +1,30 @@
+// // first way
+
+// for(let i=0;i<5;i++){
+//     console.log(i)
+// }
+
+
+// // while loop
+
+// let i=0;
+// while(i<5){
+//     i++
+//     console.log(i);
+// }
+
+
+// let arr=[1,2,3,4,5]
+// for(let ele of arr){
+//     console.log(ele);
+// }
+
+
+
+// let arr=[1,2,3,4,5]
+// arr.map((num,index)=>{
+//     console.log(num,index);
+// });
+
+
+
