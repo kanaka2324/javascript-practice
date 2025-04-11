ab03ea4ae11f858f4ee3cb01960693b60467b851
KANAKA <kanakadh2004@gmail.com>
2025-04-11 21:42:04 +0530

    commit

--- object3.js
+++ object3.js
@@ -1,4 +1,4 @@
-// 🔹 1. Object Creation
+//  1. Object Creation
 const person = {
     name: "Alice",
     age: 31,
@@ -8,62 +8,62 @@ const person = {
     }
   };
   
-//   // 🔹 2. Accessing Properties
+//   //  2. Accessing Properties
 //   console.log(person.name);       // Dot notation
 //   console.log(person["age"]);     // Bracket notation
   
-//   // 🔹 3. Updating Properties
+//   //  3. Updating Properties
 //   person.age = 32;
 //   console.log(person.age);
   
 
-//   // 🔹 4. Adding New Property
+//   //  4. Adding New Property
 //   person.city = "New York";
 //   console.log(person.city);
   
-//   // 🔹 5. Deleting Property
+//   //  5. Deleting Property
 //   delete person.isStudent;
 //   console.log(person);
   
-//   // 🔹 6. Checking Property Existence
+//   //  6. Checking Property Existence
 //   console.log("name" in person);              // true
 //   console.log(person.hasOwnProperty("age"));  // true
   
-//   // 🔹 7. Object Methods
+//   //  7. Object Methods
 //   person.greet();  // Hi, I'm Alice
   
-//   // 🔹 8. Looping Over Properties
+//   //  8. Looping Over Properties
 //   for (let key in person) {
 //     console.log(key, person[key]);
 //   }
   
-//   // 🔹 9. Object.keys() / values / entries
+//   //  9. Object.keys() / values / entries
 //   console.log(Object.keys(person));    // [ 'name', 'age', 'greet', 'city' ]
 //   console.log(Object.values(person));  // [ 'Alice', 32, [Function: greet], 'New York' ]
 //   console.log(Object.entries(person)); // [ [ 'name', 'Alice' ], ... ]
   
-//   // 🔹 10. Object.assign() – Merge Objects
+//   //  10. Object.assign() – Merge Objects
 //   const extra = { hobby: "Reading", age: 40 };
 //   const merged = Object.assign({}, person, extra);
 //   console.log(merged);
   
-//   // 🔹 11. Spread Operator – Clone / Merge
+//   //  11. Spread Operator – Clone / Merge
 //   const copy = { ...person };
 //   console.log(copy);
   
-//   // 🔹 12. Object.freeze() – Lock the object
+//   //  12. Object.freeze() – Lock the object
 //   const locked = Object.freeze({ a: 1 });
 //   locked.a = 2;
 //   console.log(locked.a); // 1
   
-//   // 🔹 13. Object.seal() – Prevent adding/removing, but allow modify
+//   //  13. Object.seal() – Prevent adding/removing, but allow modify
 //   const sealed = Object.seal({ b: 10 });
 //   sealed.b = 20;
 //   sealed.c = 30;     // Won't be added
 //   delete sealed.b;   // Won't be deleted
 //   console.log(sealed); // { b: 20 }
   
-//   // 🔹 14. Object.defineProperty()
+//   //  14. Object.defineProperty()
 //   const user = {};
 //   Object.defineProperty(user, "id", {
 //     value: 101,
